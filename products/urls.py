6e84dd1208df4e0c3f5c38b23c1f7d053bd1e7e5
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-05-08 16:38:31 +0200

    Create ProductImage

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from rest_framework import routers
 
-from products.views import ProductView
+from products.views import ProductView, ProductImageView
 
 router = routers.SimpleRouter()
+router.register('images', ProductImageView)
 router.register('', ProductView)
 urlpatterns = router.urls
--- products/views.py
+++ products/views.py
@@ -1,11 +1,17 @@
 from rest_framework import viewsets
 
 from permissions import IsAdministrator, IsAuthenticatedAndConfirmed
-from products.models import Product
-from products.serializers import ProductSerializer
+from products.models import Product, ProductImage
+from products.serializers import ProductSerializer, ProductImageSerializer
 
 
 class ProductView(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+    permission_classes = [IsAdministrator, IsAuthenticatedAndConfirmed]
+
+
+class ProductImageView(viewsets.ModelViewSet):
+    serializer_class = ProductImageSerializer
+    queryset = ProductImage.objects.all()
     permission_classes = [IsAdministrator, IsAuthenticatedAndConfirmed]
