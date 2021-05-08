02e0f76db881764a7355b3a812373788f3bff0da
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-05-08 14:21:43 +0200

    Create form in order to check if is_default=True is uniq

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
+from products.forms import ProductImageForm
 from products.models import *
 
+
+class ProductImageAdmin(admin.ModelAdmin):
+    form = ProductImageForm
+
+
 admin.site.register(Product)
-admin.site.register(ProductImage)
+admin.site.register(ProductImage, ProductImageAdmin)
 admin.site.register(FeedBack)
 admin.site.register(Availability)
 admin.site.register(Category)
