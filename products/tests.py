87d6ad9a41721eba920653655e29c01759349dfc
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-05-08 16:38:55 +0200

    Create tests for ProductImage

--- products/tests.py
+++ products/tests.py
@@ -1,6 +1,6 @@
 from rest_framework import status
 
-from products.models import Product
+from products.models import Product, ProductImage
 from users.models import UserRole
 from utils.tests import ApiTestCase
 
@@ -72,3 +72,56 @@ class ProductsTestCase(ApiTestCase):
             'price': 1.50,
             'cooking_time': 3600
         })
+
+
+class ProductImageTestCase(ApiTestCase):
+    @ApiTestCase.Decorators.create_default_user_and_log_in()
+    def test_get_product_images(self):
+        p1 = Product.objects.create(name='Product One', description='Product Description', price=1.25,
+                                    cooking_time=3600)
+        p2 = Product.objects.create(name='Product Two', description='Product Description', price=2.25,
+                                    cooking_time=1600)
+        ProductImage.objects.create(product=p1, image_url="http://image")
+        ProductImage.objects.create(product=p2, image_url="http://image", is_default=False)
+
+        response = self.client.get('/products/images/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], ProductImage.objects.all().count())
+
+    @ApiTestCase.Decorators.create_default_user_and_log_in()
+    def test_get_product_image(self):
+        p = Product.objects.create(name='Product One', description='Product Description', price=1.25,
+                                   cooking_time=3600)
+        pi_1 = ProductImage.objects.create(product=p, image_url="http://image", is_default=False)
+
+        response = self.client.get(f'/products/images/{pi_1.pk}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['id'], pi_1.pk)
+
+    @ApiTestCase.Decorators.create_default_user_and_log_in(role=UserRole.UserRoleChoice.administrator)
+    def test_create_product_image_administrator(self):
+        p = Product.objects.create(name='Product One', description='Product Description', price=1.25,
+                                   cooking_time=3600)
+
+        response = self.client.post(f'/products/images/', {
+            'image_url': 'https://image',
+            'is_default': True,
+            'is_external': False,
+            'product': p.pk
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['id'], p.pk)
+
+    @ApiTestCase.Decorators.create_default_user_and_log_in(role=UserRole.UserRoleChoice.client)
+    def test_create_product_image_client(self):
+        p = Product.objects.create(name='Product One', description='Product Description', price=1.25,
+                                   cooking_time=3600)
+
+        response = self.client.post(f'/products/images/', {
+            'image_url': 'https://image',
+            'is_default': True,
+            'is_external': False,
+            'product': p.pk
+        })
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(ProductImage.objects.all().count(), 0)
