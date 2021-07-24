2dd66f17f44b3c7353e8b961124e09ac8c339d97
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-07-24 12:15:17 +0200

    Create model order

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,17 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+from products.models import Product
+from users.models import User
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    count = models.IntegerField('count', validators=[MinValueValidator(0)])
+    price = models.FloatField('price')
+    cooking_time = models.IntegerField('cooking_time')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.product.name} for {self.user.first_name}'
