53afcf74a4d8e74da2d8532ec035b0c6dec99e3a
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-07-25 22:32:44 +0200

    Descrese product availability when it is bought

--- orders/views.py
+++ orders/views.py
@@ -1,14 +1,14 @@
-
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import mixins, status
+from rest_framework.exceptions import NotAcceptable
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from orders.models import Order
 from orders.serializers import OrderSerializer
 from permissions import IsAuthenticatedAndConfirmed
-from products.models import Product
+from products.models import Product, Availability
 
 
 class OrderView(mixins.CreateModelMixin,
@@ -33,3 +33,12 @@ class OrderView(mixins.CreateModelMixin,
         request.data['price'] = product.price
         request.data['cooking_time'] = product.cooking_time
         return super().create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        availability = Availability.objects.get(product=serializer.validated_data['product'])
+        if serializer.validated_data['count'] > availability.available:
+            raise NotAcceptable(detail='You requested more than it exists')
+        availability.available -= serializer.validated_data['count']
+        availability.is_available = bool(availability.available)
+        availability.save()
+        super().perform_create(serializer)
